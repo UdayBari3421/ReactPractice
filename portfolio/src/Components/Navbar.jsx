81dcf90d4a5760a88b6981dbcb344fea2482abb2
UdayBari3421 <udaybari2003@gmail.com>
2024-07-15 18:14:05 +0530

    portfolio add

---
+++ portfolio/src/Components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from "../assets/Uday.png";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className="border-b border-t rounded-3xl border-slate-800 before:bg-black before:w-full backdrop-blur before:content-none sticky z-10 top-0 left-10 right-10 flex items-center justify-between p-2">
+      <div className="flex flex-shrink-0 items-center">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <FaLinkedin />
+        <FaGithub />
+        <FaInstagram />
+        <FaSquareXTwitter />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
